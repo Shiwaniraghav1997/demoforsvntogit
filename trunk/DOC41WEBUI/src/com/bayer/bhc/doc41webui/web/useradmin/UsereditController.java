5f3af60ce579435ec970a0395ae05118b7ef64cb
evayd <evayd@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-06-24 14:57:45 +0000

    imports
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@49 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/useradmin/UsereditController.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/useradmin/UsereditController.java
@@ -4,29 +4,20 @@ package com.bayer.bhc.doc41webui.web.useradmin;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.bayer.bhc.doc41webui.common.exception.Doc41ExceptionBase;
 import com.bayer.bhc.doc41webui.common.exception.Doc41InvalidPasswordException;
-import com.bayer.bhc.doc41webui.common.logging.Doc41Log;
 import com.bayer.bhc.doc41webui.container.SelectionItem;
-import com.bayer.bhc.doc41webui.container.TranslationsForm;
 import com.bayer.bhc.doc41webui.container.UserEditForm;
-import com.bayer.bhc.doc41webui.domain.Translation;
 import com.bayer.bhc.doc41webui.domain.User;
 import com.bayer.bhc.doc41webui.web.AbstractDoc41Controller;
-import com.bayer.bhc.doc41webui.web.Doc41Controller;
-import com.bayer.bhc.doc41webui.web.translations.TranslationValidator;
 
 @Controller
 public class UsereditController extends AbstractDoc41Controller {
