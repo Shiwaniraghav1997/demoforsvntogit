d20280646bfade08028708a4f952d0a2225eda90
evfpu <evfpu@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-07-02 07:36:24 +0000

    remove BOE-Upsell-Constant
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@70 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/common/Doc41Constants.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/common/Doc41Constants.java
@@ -5,7 +5,6 @@ public interface Doc41Constants {
     
     public static final String USERNAME_TASK				= "*TASK*";
     public static final String USERNAME_TEST				= "*TEST*";
-    public static final String WEBMETRIX_COMPONENT_UPSELL	= "UPSEL";
     
     public static final String MONITORING_SUCCESS = "Success";
     public static final String MONITORING_FAILURE = "Failure";
