b17dbfa3702be4ee025d3a144b045fce2f2c6067
evfpu <evfpu@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-07-11 14:45:59 +0000

    DW-1 Logout
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@116 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/LogoutController.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/LogoutController.java
@@ -1,17 +1,73 @@
 package com.bayer.bhc.doc41webui.web;
 
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bayer.bhc.doc41webui.common.Doc41SessionKeys;
+import com.bayer.bhc.doc41webui.common.exception.Doc41TechnicalException;
+import com.bayer.bhc.doc41webui.common.logging.Doc41Log;
+import com.bayer.bhc.doc41webui.common.util.UserInSession;
+import com.bayer.bhc.doc41webui.domain.User;
+import com.bayer.bhc.doc41webui.integration.db.SessionDataDAO;
+import com.bayer.ecim.foundation.basic.ConfigMap;
+import com.bayer.ecim.foundation.business.sbcommon.SessionDataDC;
+
 @Controller
 public class LogoutController extends AbstractDoc41Controller {
 
+	@Autowired
+	private SessionDataDAO sessionDataDAO;
+	
 	@RequestMapping(value="/logout",method = RequestMethod.GET)
-	public String get(HttpSession session) {
-		session = null;
-		return "redirect:/login/login";
+	public String get(HttpServletRequest request, HttpServletResponse response/*HttpSession session*/) throws IOException {
+//		session = null;
+//		UserInSession.put(null);
+//		LocaleInSession.put(null);
+//		return "redirect:/login/login";
+		
+		
+		User user = (User) request.getSession().getAttribute(Doc41SessionKeys.DOC41_USER);
+    	
+    	request.getSession().invalidate();
+
+    	if(user!=null){
+    		SessionDataDC dbSessionDC = null;
+    		try {
+    			//fill user for backend
+    			UserInSession.put(user);
+    			dbSessionDC = sessionDataDAO.getSessionData(user.getCwid());
+        		if (dbSessionDC != null){
+        			sessionDataDAO.delete(dbSessionDC);
+        		}
+        		UserInSession.put(null);
+    		} catch (Doc41TechnicalException e1) {
+    			Doc41Log.get().error(this.getClass(), user.getCwid(), "DB SessionData access failed:"+e1.getMessage());
+    		e1.printStackTrace();
+    		}
+    		boolean externalUser = user.isExternalUser();
+    		@SuppressWarnings("unchecked")
+    		Map<String, String> subConfig = ConfigMap.get().getSubConfig("web", "logout");
+
+    		String url;
+    		if(externalUser){
+    			url = (String) subConfig.get("external");
+    		} else {
+    			url = (String) subConfig.get("internal");
+    		}
+
+    		if(url!=null){
+//    			response.sendRedirect(url);
+    			return "redirect:"+url;
+    		}
+    	}
+    	return "";
 	}
 }
