ec0fabe0695c562b50e32beb48f8769d6027535a
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-07-18 13:57:17 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@175 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/1_0_jenkinstest/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/useradmin/UseractivationController.java
@@ -0,0 +1,28 @@
+/**
+ *(C) Copyright 2008 Bayer AG Leverkusen, Bayer Business Services
+ * All rights reserved.
+ */
+package com.bayer.bhc.doc41webui.web.useradmin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * User activation controller.
+ * 
+ */
+public class UseractivationController extends UserlistController {
+	
+	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command, BindException errors) throws Exception {
+		
+//        getUserManagementUC().toggleUserActivation(request.getParameter("togglecwid"));
+//		
+//        return redirectOnSuccess(request, response, command, errors);
+        
+        
+        return null;
+	}
+}
---
+++ tags/1_0_jenkinstest/doc41web/database/DOC41WEB_FDTSchema-Alter.sql
@@ -0,0 +1,19 @@
+
+------------------------------------
+-- Alter-Script: CVS v1.0 -> v1.1 --
+------------------------------------
+-- New versioning
+-- Enhancements for DOC41WEB
+
+INSERT INTO  doc41web_fdt.Versions VALUES ( 'Foundation-X', 1, 1);
+
+GRANT                                 REFERENCES ON UM_USERS     TO DOC41WEB_MGR WITH GRANT OPTION;
+GRANT                                 REFERENCES ON UM_PROFILES  TO DOC41WEB_MGR WITH GRANT OPTION;
+
+------------------------------------
+-- Alter-Script: CVS v1.1 -> v1.2 --
+------------------------------------
+
+--UPDATE bco_fdt.Versions SET subVersion = 2 WHERE ( module = 'Foundation-X' ) AND ( subVersion = 1 );
+--COMMIT WORK;
+--
