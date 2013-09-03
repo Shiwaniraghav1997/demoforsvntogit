08b1fc45b74e56148f15a41fcfadc0dec41dd9b3
evayd <evayd@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-09-03 11:46:12 +0000

    pt upload refactored as specific jsp
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@249 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/documents/PTUploadController.java
@@ -0,0 +1,30 @@
+package com.bayer.bhc.doc41webui.web.documents;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.container.UploadForm;
+
+@Controller
+public class PTUploadController extends UploadController {
+
+	
+	@RequestMapping(value="/documents/ptupload",method = RequestMethod.GET)
+	public UploadForm get(@RequestParam() String type,@RequestParam(required=false) String fileid) throws Doc41BusinessException{
+		return super.get(type, fileid);
+	}
+	
+	@RequestMapping(value="/documents/ptuploadpost",method = RequestMethod.POST)
+	public String postUpload(@ModelAttribute UploadForm uploadForm,BindingResult result) throws Doc41BusinessException { //ggf. kein modelattribute wegen sessionattribute
+		return super.postUpload(uploadForm, result);
+	}
+	
+	protected String getFailedURL() {
+		return "/documents/ptupload";
+	}
+}
