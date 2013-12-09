3529332d7be99603ee6baf29b1bccbbc6cc3b324
evayd <evayd@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2013-12-09 13:44:44 +0000

    upload variants (not used)
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@413 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/service/httpclient/HttpClientService.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/service/httpclient/HttpClientService.java
@@ -60,6 +60,66 @@ public class HttpClientService {
 		}
 	}
 	
+//	public void uploadDocumentToUrl(URI putUrl, File localFile,final String mimeType) throws Doc41ServiceException {
+//		try {
+//			HttpClient client = new DefaultHttpClient();
+//		    HttpPut httpPut = new HttpPut(putUrl);
+//
+//		    MultipartEntity mpEntity = new MultipartEntity(
+//		            HttpMultipartMode.BROWSER_COMPATIBLE);
+//		    FileBody contentBody = new FileBody(localFile, mimeType);
+//		    mpEntity.addPart("data", contentBody);
+//		    httpPut.setEntity(mpEntity);
+//		    HttpResponse httpResponse = null;
+//		        httpResponse = client.execute(httpPut);
+//		} catch (IOException e) {
+//			throw new Doc41ServiceException("uploadDocumentToUrl", e);
+//		} 
+//	}
+	
+//	public void uploadDocumentToUrl(URI putUrl, File localFile,final String mimeType) throws Doc41ServiceException {
+//		InputStream inputStream = null;
+//		OutputStream outputStream = null;
+//		byte[] buffer = new byte[1024];
+//		try {
+//			inputStream = new FileInputStream(localFile);
+//
+//			URL url = putUrl.toURL();
+//			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//			connection.setRequestProperty("Content-Type", mimeType);
+//			connection.setRequestMethod("PUT");
+//			connection.setDoOutput(true);
+//
+//			outputStream = connection.getOutputStream();
+//			int read;
+//			while ((read = inputStream.read(buffer)) != -1) {
+//				outputStream.write(buffer, 0, read);
+//			}
+//
+//			outputStream.flush();
+//			outputStream.close();
+//			int code = connection.getResponseCode();
+//			System.out.println("put response code: "+code);
+//		} catch (IOException e) {
+//			throw new Doc41ServiceException("uploadDocumentToUrl", e);
+//		} finally {
+//			if(outputStream!=null){
+//				try {
+//					outputStream.close();
+//				} catch (IOException e) {
+//					throw new Doc41ServiceException("uploadDocumentToUrl", e);
+//				}
+//			}
+//			if(inputStream!=null){
+//				try {
+//					inputStream.close();
+//				} catch (IOException e) {
+//					throw new Doc41ServiceException("uploadDocumentToUrl", e);
+//				}
+//			}
+//		}
+//	}	
+	
 	public String downloadDocumentToResponse(URI getUrl, final HttpServletResponse targetResponse,final String docId,final String fileName) throws Doc41ServiceException{
 		final ResponseExtractor<String> responseExtractor = new ResponseExtractor<String>() {
 			
