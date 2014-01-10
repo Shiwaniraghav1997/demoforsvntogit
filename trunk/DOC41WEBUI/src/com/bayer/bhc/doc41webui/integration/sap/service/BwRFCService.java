582d7346d707e42766a2e3e562f10b502b85b038
evayd <evayd@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2014-01-10 09:12:24 +0000

    find docs 2
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@460 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/integration/sap/service/BwRFCService.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/integration/sap/service/BwRFCService.java
@@ -15,7 +15,7 @@ public class BwRFCService extends AbstractSAPJCOService {
 	private static final String RFC_NAME_FIND_DOCS = "FindDocs";
 
 
-	public List<HitListEntry> findDocs(String d41id,String sapObj,List<String> objectIds,Map<String, String> attributeValues,int maxResults,boolean maxVersionOnly)
+	public List<HitListEntry> findDocs(String d41id,String sapObj,List<String> objectIds,Map<String, String> attributeValues,int maxResults,boolean maxVersionOnly, Map<Integer, String> seqToKey)
 	 throws Doc41ServiceException{
 		List<Object> params = new ArrayList<Object>();
 		params.add(d41id);
@@ -24,6 +24,7 @@ public class BwRFCService extends AbstractSAPJCOService {
 		params.add(maxResults);
 		params.add(maxVersionOnly);
 		params.add(attributeValues);
+		params.add(seqToKey);
 		
 		List<HitListEntry> result = performRFC(params, RFC_NAME_FIND_DOCS);
 		return result;
