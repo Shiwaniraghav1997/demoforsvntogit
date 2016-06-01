59a97c632d8433bfa85606edb0eab1c6c3a2883a
imwif <imwif@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2016-06-01 16:48:13 +0000

    moved implementation of JUnitTests to extra Impl Classes in main classpath, for share tests with SupportConsole
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@717 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src-test/com/bayer/bhc/doc41webui/Doc41TestCase.java
+++ trunk/DOC41WEBUI/src-test/com/bayer/bhc/doc41webui/Doc41TestCase.java
@@ -1,6 +1,5 @@
 package com.bayer.bhc.doc41webui;
 
-import java.util.Locale;
 
 import junit.framework.TestCase;
 
@@ -13,11 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //import org.springframework.web.context.request.RequestContextHolder;
 //import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.bayer.bhc.doc41webui.common.Doc41Constants;
 import com.bayer.bhc.doc41webui.common.config.FoundationXmlContextLoader;
-import com.bayer.bhc.doc41webui.common.util.LocaleInSession;
-import com.bayer.bhc.doc41webui.common.util.UserInSession;
-import com.bayer.bhc.doc41webui.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader=FoundationXmlContextLoader.class,locations={"classpath:cfg/spring/applicationContext.xml",
@@ -29,14 +24,7 @@ public class Doc41TestCase extends TestCase {
 	@BeforeClass
 	public static void setUserInSession() {
 		System.out.println("@BeforeClass");
-//		RequestAttributes attributes = new ServletRequestAttributes(new MockHttpServletRequest()); 
-//		RequestContextHolder.setRequestAttributes(attributes);
-		User tmpUser	= new User();
-		tmpUser.setReadOnly(Boolean.FALSE);
-		tmpUser.setCwid(Doc41Constants.USERNAME_TEST);
-		UserInSession.put(tmpUser);
-		Locale tmpLocale= new Locale("en", "US");
-		LocaleInSession.put(tmpLocale);
-		System.setProperty("spring.profiles.active", "mock");
+		com.bayer.bhc.doc41webui.web.test.Doc41TestCaseImpl.setUserInSession();
+		System.setProperty("spring.profiles.active", "mock"); // ???????
 	}		
 }
--- trunk/DOC41WEBUI/src-test/com/bayer/bhc/doc41webui/integration/sap/docservice/DocServiceUploadTestClient.java
+++ trunk/DOC41WEBUI/src-test/com/bayer/bhc/doc41webui/integration/sap/docservice/DocServiceUploadTestClient.java
@@ -1,94 +1,16 @@
 package com.bayer.bhc.doc41webui.integration.sap.docservice;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.InputStreamBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.bayer.bhc.doc41webui.web.test.DocServiceUploadTestClientImpl;
 
 public class DocServiceUploadTestClient {
     
-    private static final String URL_PREFIX = "http://localhost:9080/doc41";
-
     public static void main(String[] args) throws ClientProtocolException, IOException {
-        DocServiceUploadTestClient client = new DocServiceUploadTestClient();
-        
-        String fileName="rfcReport.pdf";
-        InputStream fileInputStream = null;
-        try{
-        fileInputStream = new FileInputStream("C:\\eclipse\\BOEAH\\workspace\\DOC41WEBUI\\docs\\"+fileName);
-        String type="AWB";
-        String vendorNumber="0007150493";
-        String refnumber="0060006010";
-        
-        HttpEntity result = client.testUpload(URL_PREFIX,fileInputStream,fileName,type,vendorNumber,refnumber);
-        System.out.println(""+result);
-        } finally {
-            if(fileInputStream!=null){
-                fileInputStream.close();
-            }
-        }
+        DocServiceUploadTestClientImpl dsuci = new DocServiceUploadTestClientImpl();
+        new DocServiceUploadTestClientImpl().run(dsuci.URL_PREFIX, dsuci.VENDOR, dsuci.REF_NO, dsuci.UPL_PATH, dsuci.UPL_FILE, dsuci.TYPE);
     }
     
-    private HttpEntity testUpload(String urlPrefix, InputStream fileInputStream,
-            String fileName, String type, String vendorNumber,
-            String refnumber) throws ClientProtocolException, IOException {
-
-        String url=getUploadUrl(urlPrefix);
-        HttpPost httpPost = new HttpPost(url);
-        addUser(httpPost);
-        System.out.println("Executing request " + httpPost.getRequestLine());
-        
-        DefaultHttpClient httpclient = new DefaultHttpClient();
-        try{
-            
-          
-            MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-            entity.addPart("fileName", new StringBody(fileName));
-            entity.addPart("type", new StringBody(type));
-            entity.addPart("vendorNumber", new StringBody(vendorNumber));
-            entity.addPart("objectId", new StringBody(refnumber));
-            
-            InputStreamBody fileBody = new InputStreamBody(fileInputStream, "application/octet-stream",fileName);
-            entity.addPart("file", fileBody);
-
-            httpPost.setEntity(entity);
-            HttpResponse response = httpclient.execute(httpPost);
-            StatusLine statusLine = response.getStatusLine();
-            if (statusLine.getStatusCode() >= 300) {
-                throw new HttpResponseException(
-                        statusLine.getStatusCode(),
-                        statusLine.getReasonPhrase());
-            }
-            HttpEntity result = response.getEntity();
-            return result;
-            
-            
-        } finally {
-            httpPost.releaseConnection();
-        }
-    }
-
-
-    private void addUser(HttpRequestBase request) {
-        request.setHeader("docservice-user", "ABCDE");
-        request.setHeader("docservice-role", "doc41_carr");
-    }
-
-
-    private String getUploadUrl(String urlPrefix) {
-        return urlPrefix+"/docservice/sdupload";
-    }
-
 }
