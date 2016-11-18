f4710f3380d667ca66cb6159b178841058266fc5
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2016-11-18 14:00:29 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@792 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/1_0_84/DOC41WEBUI/src/com/bayer/bhc/doc41webui/domain/SapVendor.java
@@ -0,0 +1,40 @@
+package com.bayer.bhc.doc41webui.domain;
+
+import com.bayer.ecim.foundation.basic.StringTool;
+
+public class SapVendor extends DomainObject {
+
+	private static final long serialVersionUID = 2511145389307051601L;
+	private String number;
+	private String name1;
+	private String name2;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName1() {
+		return name1;
+	}
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+	public String getName2() {
+		return name2;
+	}
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+	@Override
+	public String toString() {
+		return "SapVendor [number=" + number + ", name1=" + name1 + ", name2="
+				+ name2 + "]";
+	}
+	public String getLabel(){
+		return ""+number+" "+StringTool.nullToEmpty(name1)+" "+StringTool.nullToEmpty(name2);
+	}
+	
+	
+}
