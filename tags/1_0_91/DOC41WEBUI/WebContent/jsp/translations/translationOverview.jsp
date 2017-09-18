6bd094a6bd1068b49bfee293950d6bad8af51d21
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2017-09-18 10:14:56 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@816 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/1_0_91/DOC41WEBUI/WebContent/jsp/translations/translationOverview.jsp
@@ -0,0 +1,74 @@
+<%@taglib prefix="doc41" uri="doc41-tags" %><doc41:layout activePage="${pageContext.request.servletPath}"
+jspName="translationOverview" component="TADMN"
+activeTopNav="maintenance" 	activeNav="translations"
+title="Translations">
+<%@taglib prefix="c" 		uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="form" 	uri="http://www.springframework.org/tags/form" %>
+<%@taglib prefix="spring"	uri="http://www.springframework.org/tags" %>
+
+<script type="text/javascript">
+<!--
+tsheaders = {
+          7: {
+            sorter: false,
+            filter: false
+          },
+          8: {
+	      	sorter: false,
+	      	filter: false
+	      }
+};
+
+//-->
+</script>
+<script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/doc41tablesorter.js"></script>
+    <div class="portlet-body">
+		<c:if test="${editable}">
+			<div class="portlet-section-header">
+		    	<a class="portlet-form-button" href='translationAdd'><doc41:translate label="ButtonCreate"/></a>
+			</div>
+		</c:if>
+
+
+		<div class="portlet-section-body">
+		
+		<div class="portlet-section-body">
+		
+		<doc41:pager/>
+		<table class="tablesorter" id="doc41table">
+			<colgroup>
+		    	<col width="5%"/>
+		    	<col width="10%"/>
+		    	<col width="10%"/>
+		    	<col width="20%"/>
+		    	<col width="5%"/>
+		    	<col width="5%"/>
+		    	<col width="41%"/>
+		    	<col width="2%"/>
+		    	<col width="2%"/>
+		    </colgroup>		
+			<thead>
+		    <tr>
+			      
+			       <!-- thead text will be updated from the JSON; make sure the number of columns matches the JSON data -->
+			       <!-- header update currently disabled to put names in the jsp instead of in java  -->
+			    <th data-placeholder="exact only"><doc41:translate label="Mandant" /></th>
+				<th data-placeholder="exact only"><doc41:translate label="Component" /></th>
+				<th data-placeholder="exact only"><doc41:translate label="Page" /></th>
+				<th><doc41:translate label="TagName" /></th>
+				<th data-placeholder="exact only"><doc41:translate label="Language" /></th>
+				<th data-placeholder="exact only"><doc41:translate label="Country" /></th>
+				<th><doc41:translate label="TagValue" /></th>
+				<th>&nbsp;</th>
+				<th>&nbsp;</th>
+		    </tr>
+		  	</thead>
+		
+		  <tbody> <!-- tbody will be loaded via JSON -->
+		  </tbody>
+		</table>
+		<doc41:pager/>
+		</div>
+	</div>
+</div>
+</doc41:layout>
---
+++ tags/1_0_91/DOC41WEBUI/src/com/bayer/bhc/doc41webui/integration/sap/rfc/CheckArtworkLayoutForVendorRFC.java
@@ -0,0 +1,77 @@
+package com.bayer.bhc.doc41webui.integration.sap.rfc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bayer.bhc.doc41webui.common.logging.Doc41Log;
+import com.bayer.ecim.foundation.basic.StringTool;
+import com.bayer.ecim.foundation.sap3.SAPException;
+import com.sap.conn.jco.JCoFunction;
+import com.sap.conn.jco.JCoParameterList;
+
+/**
+ * CheckArtworkLayoutForVendorRFC
+ * Check if there is already Artwork Layout of same material number (TODO). Currently checks only, if ther is any Artwork Layout for this vendor.
+ * currently implemented parameters: vendor (partner) number, sap (doc) type id
+ * further required parameter: material number
+ * @author ???, IMWIF
+ */
+public class CheckArtworkLayoutForVendorRFC extends AbstractDoc41RFC<String>{
+	private static final String IN_VENDOR = "IV_VENDOR";
+	private static final String IN_DOC_TYPE = "IV_DOCTYPE_ID";
+	private static final String OUT_RETURNCODE = "EV_RETURN";
+	
+	private static final String RETURNCODE_OK = "0";
+	private static final String RETURNCODE_NOT_FOUND = "4";
+
+	@Override
+	public void prepareCall(JCoFunction pFunction, List<?> pInputParms)
+			throws SAPException {
+		Doc41Log.get().debug(this, null, "prepareCall():ENTRY");
+    	
+        if (pFunction != null) {
+            if (pInputParms != null) {
+                String vendorNumber = (String) pInputParms.get(0);
+                String doctype = (String) pInputParms.get(1);
+                
+                JCoParameterList sapInput = pFunction.getImportParameterList();
+				
+				sapInput.setValue(IN_VENDOR,vendorNumber);
+				sapInput.setValue(IN_DOC_TYPE,doctype);
+		        Doc41Log.get().debug(this, null, RFCFunctionDumper.dumpFunction(pFunction));
+            } else {
+                throw new SAPException(
+                        "CheckArtworkLayoutForVendorRFC pInputParms list is null", null);
+            }
+        } else {
+            throw new SAPException(
+                    "CheckArtworkLayoutForVendorRFC pFunction list is null", null);
+        }
+        Doc41Log.get().debug(this, null, "prepareCall():EXIT");
+	}
+
+	@Override
+	public List<String> processResult(JCoFunction pFunction)
+			throws SAPException {
+		Doc41Log.get().debug(CheckArtworkLayoutForVendorRFC.class, null, "processResult():ENTRY");
+		ArrayList<String> mResult = new ArrayList<String>();
+        if (pFunction != null) {
+            Doc41Log.get().debug(this, null, RFCFunctionDumper.dumpFunction(pFunction));
+            JCoParameterList exportParameterList = pFunction.getExportParameterList();
+            String returnCode = exportParameterList.getString(OUT_RETURNCODE);
+            mResult.add(mapReturnCodeToTag(returnCode));
+        }
+        Doc41Log.get().debug(CheckArtworkLayoutForVendorRFC.class, null, "processResult():EXIT");
+        return mResult;
+	}
+	
+	private String mapReturnCodeToTag(String returnCode) {
+		if(StringTool.equals(returnCode, RETURNCODE_OK)){
+			return null;
+		} else if(StringTool.equals(returnCode, RETURNCODE_NOT_FOUND)){
+			return "NoDocumentsFound";
+		}
+		return "UnknownReturnCode";
+	}
+
+}
