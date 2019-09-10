5df450cb5ab31fe0144832bafbe1c6719331295e
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2019-09-10 13:37:42 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@948 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/1_3_0/DOC41WEBUI/src/com/bayer/bhc/doc41webui/common/logging/Doc41Log.java
@@ -0,0 +1,192 @@
+/**
+ * (C) Copyright 2008 Bayer AG Leverkusen, Bayer Business Services
+ * All rights reserved.
+ */
+package com.bayer.bhc.doc41webui.common.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bayer.bhc.doc41webui.common.Doc41SessionKeys;
+import com.bayer.bhc.doc41webui.common.util.UserInSession;
+import com.bayer.ecim.foundation.basic.Dbg;
+import com.bayer.ecim.foundation.basic.NestingException;
+import com.bayer.ecim.foundation.basic.NestingExceptions;
+
+/**
+ * Foundation specific logger class for CargoCockpit.
+ * 
+ * @author ezzqb
+ * @id $Id: Doc41LoggerImpl.java,v 1.2 2012/02/22 14:16:09 ezzqc Exp $
+ */
+public class Doc41Log {
+
+
+    private static Doc41Log instance;
+        
+    public static synchronized Doc41Log get() {
+        if (instance == null) {
+            instance = new Doc41Log();
+        }
+        return instance;
+    }
+    
+
+    /** the error channel */
+    private static final int ERROR = Dbg.ERROR;
+
+    /** the info channel */
+    private static final int INFO = Dbg.INFO;
+
+    /** the dump channel */
+    private static final int BIGINFO = Dbg.BIGINFO;
+
+    /** the WARNING channel */
+    private static final int WARNING = Dbg.WARNING;
+
+    /** the logging channel */
+    private static final int LOGGING = Dbg.LOGGING;
+    
+    
+    /**
+     * Looks, if debug channel is active - this is NOT static/final!!!
+     */
+    public boolean isDebugActive() {
+        return Dbg.get().isLogicalChannelActive(INFO);
+    }
+    
+    /**
+     * Logs debug message.
+     * @param clazz   The Log Class
+     * @param user    The User
+     * @param msg     The Debug message
+     */
+    public void debug(Object clazz, String user, Object msg) {
+        Dbg.get().println(INFO, clazz, (user == null) ? UserInSession.getCwid() : user, msg);
+    }
+
+    /**
+     * Looks, if debug channel is active - this is NOT static/final!!!
+     */
+    public boolean isDumpActive() {
+        return Dbg.get().isLogicalChannelActive(BIGINFO);
+    }
+    
+    /**
+     * Logs debug(dump) message (extra channel for heavy debug, dumps).
+     * @param clazz   The Log Class
+     * @param user    The User
+     * @param msg     The Debug message
+     */
+    public void dump(Object clazz, String user, Object msg) {
+        Dbg.get().println(BIGINFO, clazz, (user == null) ? UserInSession.getCwid() : user, msg);
+    }
+
+    /**
+     * Warning messages.
+     * @param clazz   The Log Class
+     * @param user    The User
+     * @param msg     The error message
+     */
+    public void warning(Object clazz, String user, Object msg) {
+        Dbg.get().println(WARNING, clazz, (user == null) ? UserInSession.getCwid() : user, msg);
+    }
+
+    /**
+     * Logs Error Message.
+     * @param clazz   The Log Class
+     * @param user    The User
+     * @param msg     The error message
+     */
+    public void error(Object clazz, String user, Object msg) {
+    	//to log stacktrace
+    	if(msg instanceof Throwable && !(msg instanceof NestingExceptions)){
+    		Throwable ex = (Throwable) msg;
+    		new NestingException(ex.getMessage(), ex);
+    	}
+        Dbg.get().println(ERROR, clazz, (user == null) ? UserInSession.getCwid() : user, msg);
+    }
+
+    /**
+     * Controllers specific method to log webmetrix data in Webmetrix table.
+     * @param pRequest          The PortletletRequest
+     * @param handler         	The PortletHandlerObject
+     * @param modelAndView      The ModelAndView
+     */
+	public void logWebMetrix(HttpServletRequest pRequest, Object handler, ModelAndView modelAndView) {
+	    String usr = UserInSession.getCwid();
+		String jspName =modelAndView.getViewName();
+		String action = (String)pRequest.getSession().getAttribute(Doc41SessionKeys.DOC41_LAST_RENDERED_CTRL);
+					
+		Doc41LogEntry pObj = new Doc41LogEntry(usr, usr, null, action, jspName, null, null, null, null, null, null, null, null);
+		Dbg.get().println(LOGGING, handler, pRequest.getRemoteUser(),pObj);
+	}
+
+	/**
+	 * Print (debug) a message one time, but then no more (until next logfile change - prevent spamming)
+	 * @param clazz
+	 * @param user
+	 * @param mMessage
+	 */
+	public void debugMessageOnce(Object clazz, String user, String mMessage ) {
+	    user = (user == null) ? UserInSession.getCwid() : user;
+	    Dbg.get().dbgLogMessageOnce(INFO, 0, clazz, user, user, "", mMessage);
+	}
+	
+    /**
+     * Print (warn) a message one time, and on further occurence only to dump channel(until next logfile change - prevent spamming)
+     * @param clazz
+     * @param user
+     * @param mMessage
+     */
+    public void warnMessageOnce(Object clazz, String user, String mMessage ) {
+        user = (user == null) ? UserInSession.getCwid() : user;
+        Dbg.get().dbgLogMessageOnce(WARNING, BIGINFO, clazz, user, user, "", mMessage);
+    }
+    
+
+    /**
+     * Controllers specific method to log webmetrix data in Webmetrix table.
+     * @param pRequest          The PortletletRequest
+     * @param handler         	The PortletHandlerObject
+     * @param URI      The requested URI
+     */
+	public void logWebMetrix(HttpServletRequest pRequest, Object handler, String anURI) {
+		String usr = UserInSession.getCwid();
+		String action = "n/a";
+		try {
+		    action = (String)pRequest.getSession().getAttribute(Doc41SessionKeys.DOC41_LAST_RENDERED_CTRL);
+		} catch (java.lang.IllegalStateException e) {
+		    action = "???";
+		    Doc41Log.get().debug(this, usr, "java.lang.IllegalStateException: "+e.getMessage());
+		}
+		Doc41LogEntry pObj = new Doc41LogEntry(usr, usr, "DOC41WEB", action, anURI, null, null, null, null, null, null, null, null);
+		Dbg.get().println(LOGGING, handler, pRequest.getRemoteUser(),pObj);
+	}
+
+	/**
+     * Common method to log webmetrix data
+     * @param pRequest          The PortletletRequest
+     * @param handler         	The PortletHandlerObject
+     */
+	public void logWebMetrix(HttpServletRequest pRequest, Object handler) {
+		String jspName = null;
+		String usr = UserInSession.getCwid();
+		String action = (String)pRequest.getSession().getAttribute(Doc41SessionKeys.DOC41_LAST_RENDERED_CTRL);
+		jspName = (String)pRequest.getSession().getAttribute(Doc41SessionKeys.DOC41_LAST_RENDERED_VIEW);
+		Doc41LogEntry pObj = new Doc41LogEntry(usr, usr, null, action, jspName, null, null, null, null, null, null, null, null);
+		Dbg.get().println(LOGGING, handler, pRequest.getRemoteUser(),pObj);
+	}
+	
+	
+	/**
+	 * Common method to log webmetrix data
+	 * @param handler
+	 * @param logEntry
+	 * @param usr
+	 */
+	public void logWebMetrix(Object clazz, Doc41LogEntry logEntry, String user) {
+		Dbg.get().println(LOGGING, clazz, (user == null) ? UserInSession.getCwid() : user, logEntry);
+	}
+}
---
+++ tags/1_3_0/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/pm/PMSupplierDownloadDocumentType.java
@@ -0,0 +1,141 @@
+package com.bayer.bhc.doc41webui.usecase.documenttypes.ptms.pm;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.validation.Errors;
+
+import com.bayer.bhc.doc41webui.common.Doc41Constants;
+import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.common.util.Doc41Utils;
+import com.bayer.bhc.doc41webui.common.util.Doc41ValidationUtils;
+import com.bayer.bhc.doc41webui.usecase.DocumentUC;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.CheckForDownloadResult;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
+
+public abstract class PMSupplierDownloadDocumentType implements DownloadDocumentType {
+	// kgs-mode:
+	public static final String VIEW_ATTRIB_PO_NUMBER = "poNumber";
+
+	// global:
+	public static final String VIEW_ATTRIB_DOC_TYPE = "docType";
+
+	// dirs-mode only:
+	public static final String VIEW_ATTRIB_FILENAME = "FILENAME";
+
+	@Override
+	public boolean hasCustomerNumber() {
+		return false;
+	}
+
+	@Override
+	public boolean hasVendorNumber() {
+		return true;
+	}
+
+	@Override
+	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber, String vendorNumber, String objectId, String customVersion, Date timeFrame, Map<String, String> attributeValues, Map<String, String> viewAttributes) throws Doc41BusinessException {
+		Doc41ValidationUtils.checkMaterialNumber(objectId, "objectId", errors, true);
+		Map<String, String> additionalAttributes = new HashMap<String, String>();
+		if (!errors.hasErrors()) {
+			List<String> deliveryCheck = documentUC.checkMaterialForVendor(vendorNumber, objectId, customVersion, timeFrame);
+			if (deliveryCheck.get(0) != null) {
+				errors.reject("" + deliveryCheck);
+			}
+			if (deliveryCheck.size() >= 2) {
+				additionalAttributes.put("IV_PLANT_BOM", deliveryCheck.get(1));
+				additionalAttributes.put("IV_VERID_BOM", customVersion);
+				additionalAttributes.put("IV_TIMEFRAME_DATE", Doc41Utils.convertDateToString(timeFrame));
+			}
+		}
+		return new CheckForDownloadResult(additionalAttributes, null);
+
+	}
+
+	@Override
+	public int getObjectIdFillLength() {
+		return Doc41Constants.FIELD_SIZE_MATNR;
+	}
+
+	@Override
+	public Set<String> getExcludedAttributes() {
+		return Collections.emptySet();
+	}
+
+	// kgs-mode / dirs-mode:
+
+	/**
+	 * Flag to determine, if document uses DIRS store.
+	 * 
+	 * @return true, if using DIRS.
+	 */
+	@Override
+	public boolean isDirs() {
+		return false; // true;
+	}
+
+	/**
+	 * Flag to determine, if document uses KGS store.
+	 * 
+	 * @return true, if using KGS.
+	 */
+	@Override
+	public boolean isKgs() {
+		return true; // false;
+	}
+
+	/**
+	 * Set the profile type for download permissions (DOC_SD/QM/LS/PM), see
+	 * DocumentType.GROUP_* constants.
+	 * 
+	 * @param pPermType
+	 */
+	public void setDownloadPermissionType(String pPermType) {
+		cDownloadPermType = pPermType;
+	}
+
+	/**
+	 * Get the profile type for download permissions (DOC_SD/QM/LS/PM), see
+	 * DocumentType.GROUP_* constants.
+	 * 
+	 * @return
+	 */
+	public String getDownloadPermissionType() {
+		return cDownloadPermType;
+	}
+
+	/** Type of the Profile for Download Permissions. */
+	String cDownloadPermType = null;
+
+	/**
+	 * The permissions allowing download for all documents of the same document
+	 * group (= permission type). Take care, permission type is DOC_xx,
+	 * corresponding permission is DOC_GLO_xx...
+	 * 
+	 * @return the permission code, may be null, if this document is not allowed to
+	 *         download by group membership.
+	 */
+	@Override
+	public String getGroupPermissionDownload() {
+		return GROUP_PM_PERM_DOWNL;
+	}
+
+	/**
+	 * Get the profile type for download permissions (DOC_SD/QM/LS/PM), see
+	 * DocumentType.GROUP_* constants.
+	 * 
+	 * @return
+	 */
+	public String getGroup() {
+		return GROUP_PM;
+	}
+
+	@Override
+	public boolean isNotificationEMailHidden() {
+		return false;
+	}
+}
