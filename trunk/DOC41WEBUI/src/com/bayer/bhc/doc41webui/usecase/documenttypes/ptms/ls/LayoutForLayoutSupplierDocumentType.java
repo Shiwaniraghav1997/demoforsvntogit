3dfe62fdd231948ddd08b3ac79369bb16b1ec8ad
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2021-10-04 06:38:34 +0000

    BOM development
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@974 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/ls/LayoutForLayoutSupplierDocumentType.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/ls/LayoutForLayoutSupplierDocumentType.java
@@ -1,7 +1,14 @@
 package com.bayer.bhc.doc41webui.usecase.documenttypes.ptms.ls;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.validation.Errors;
+
+import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.usecase.DocumentUC;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.CheckForDownloadResult;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.UploadDocumentType;
 
@@ -43,6 +50,16 @@ public class LayoutForLayoutSupplierDocumentType extends LayoutSupplierDocumentT
 	@Override
 	public boolean isNotificationEMailHidden() {
 		return false;
+	}
+//	elerj by force added
+
+	@Override
+	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber,
+			String vendorNumber, String objectId, String customVersion, Date timeFrame,
+			Map<String, String> attributeValues, Map<String, String> viewAttributes, String puchaseOrder)
+			throws Doc41BusinessException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/ls/PackMatSpecForLayoutSupplierDocumentType.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/ls/PackMatSpecForLayoutSupplierDocumentType.java
@@ -1,7 +1,14 @@
 package com.bayer.bhc.doc41webui.usecase.documenttypes.ptms.ls;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.validation.Errors;
+
+import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.usecase.DocumentUC;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.CheckForDownloadResult;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
 
 public class PackMatSpecForLayoutSupplierDocumentType extends LayoutSupplierDocumentType
@@ -37,6 +44,16 @@ implements DownloadDocumentType{
 	@Override
 	public boolean isNotificationEMailHidden() {
 		return false;
+	}
+
+//	elerj by force added 
+	@Override
+	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber,
+			String vendorNumber, String objectId, String customVersion, Date timeFrame,
+			Map<String, String> attributeValues, Map<String, String> viewAttributes, String puchaseOrder)
+			throws Doc41BusinessException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
