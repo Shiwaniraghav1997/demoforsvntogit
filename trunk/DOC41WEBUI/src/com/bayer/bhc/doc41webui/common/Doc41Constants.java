8bdee82ffb3cee087e0bf96a3ccf56a3e3716aaa
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2021-10-04 06:15:54 +0000

    BOM development: Constant
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@969 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/common/Doc41Constants.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/common/Doc41Constants.java
@@ -165,5 +165,7 @@ public final class Doc41Constants {
 	/**
 	 * Private constructor to prevent instantiation.
 	 */
+//	purchase order elerj
+	public static final int PM_DOCUMENT_SUBTYPE_PURCHASE_ORDER = 1;
 	private Doc41Constants() {}
 }
