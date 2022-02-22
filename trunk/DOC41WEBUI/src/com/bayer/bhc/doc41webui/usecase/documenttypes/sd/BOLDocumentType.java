c1079a480371095df6809f613516bf6db6ec28c6
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-02-22 08:38:04 +0000

    Rollback the BOM YBM changes
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1011 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/sd/BOLDocumentType.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/sd/BOLDocumentType.java
@@ -1,14 +1,7 @@
 package com.bayer.bhc.doc41webui.usecase.documenttypes.sd;
 
-import java.util.Date;
-import java.util.Map;
 import java.util.Set;
 
-import org.springframework.validation.Errors;
-
-import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
-import com.bayer.bhc.doc41webui.usecase.DocumentUC;
-import com.bayer.bhc.doc41webui.usecase.documenttypes.CheckForDownloadResult;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.DirectDownloadDocumentType;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
 import com.bayer.bhc.doc41webui.usecase.documenttypes.UploadDocumentType;
@@ -43,16 +36,6 @@ public class BOLDocumentType extends SDDocumentType implements DownloadDocumentT
 
 	@Override
 	public Set<String> getMandatoryAttributes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	//ELERj by force added
-	@Override
-	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber,
-			String vendorNumber, String objectId, String customVersion, Date timeFrame,
-			Map<String, String> attributeValues, Map<String, String> viewAttributes, String puchaseOrder)
-			throws Doc41BusinessException {
 		// TODO Auto-generated method stub
 		return null;
 	}
