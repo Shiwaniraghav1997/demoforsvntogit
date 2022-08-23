d396924ce8890a6581a3d563c3cd1f6439bc6c28
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-08-23 07:34:44 +0000

    OTT: 107550 :validation for PO
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1028 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/pm/PMSupplierDownloadDocumentType.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/pm/PMSupplierDownloadDocumentType.java
@@ -1,5 +1,6 @@
 package com.bayer.bhc.doc41webui.usecase.documenttypes.ptms.pm;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import org.springframework.validation.Errors;
 
 import com.bayer.bhc.doc41webui.common.Doc41Constants;
 import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.common.exception.Doc41ServiceException;
 import com.bayer.bhc.doc41webui.common.util.Doc41Utils;
 import com.bayer.bhc.doc41webui.common.util.Doc41ValidationUtils;
 import com.bayer.bhc.doc41webui.usecase.DocumentUC;
@@ -38,21 +40,55 @@ public abstract class PMSupplierDownloadDocumentType implements DownloadDocument
 	}
 
 	@Override
-	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber, String vendorNumber, String objectId, String customVersion, Date timeFrame, Map<String, String> attributeValues, Map<String, String> viewAttributes) throws Doc41BusinessException {
-		Doc41ValidationUtils.checkMaterialNumber(objectId, "objectId", errors, true);
+	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber, String vendorNumber, String objectId, String customVersion, Date timeFrame, Map<String, String> attributeValues, Map<String, String> viewAttributes, int subType,String purchaseOrder) throws Doc41BusinessException {
+		boolean flag=true;
+		List<String> deliveryCheck = null;
+		ArrayList<String> pv_list = new ArrayList<String>();
+		if(subType==1)
+		{
+			flag=false;	
+			Doc41ValidationUtils.checkPurchaseOrder(purchaseOrder, "purchaseOrder", errors, 10);
+		}
+		Doc41ValidationUtils.checkMaterialNumber(objectId, "objectId", errors, flag);
+	
 		Map<String, String> additionalAttributes = new HashMap<String, String>();
 		if (!errors.hasErrors()) {
-			List<String> deliveryCheck = documentUC.checkMaterialForVendor(vendorNumber, objectId, customVersion, timeFrame);
+			
+			if(subType==1) {
+				try {
+					 deliveryCheck = documentUC.checkSpecification(vendorNumber, purchaseOrder);
+					/* System.out.println("deliveryCheck for specification::"+deliveryCheck.get(0));*/
+					 if (deliveryCheck.get(0) != null) {
+							errors.reject("" + deliveryCheck.get(0));
+						}
+
+						if (deliveryCheck.size() >= 2) {
+							for(int i=2;i<deliveryCheck.size();i++) {
+								if(deliveryCheck.get(i).length()==4){
+									pv_list.add( deliveryCheck.get(i));
+									additionalAttributes.put("IV_VERID_BOM", deliveryCheck.get(i));	
+								}
+								}}
+				} catch (Doc41ServiceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}else {
+//				System.out.println("else part  for checkSpecification RFC");
+			 deliveryCheck = documentUC.checkMaterialForVendor(vendorNumber, objectId, customVersion, timeFrame);
 			if (deliveryCheck.get(0) != null) {
 				errors.reject("" + deliveryCheck.get(0));
 			}
 			if (deliveryCheck.size() >= 2) {
+//				System.out.println("to check size for deliverycheck");
 				additionalAttributes.put("IV_PLANT_BOM", deliveryCheck.get(1));
 				additionalAttributes.put("IV_VERID_BOM", customVersion);
 				additionalAttributes.put("IV_TIMEFRAME_DATE", Doc41Utils.convertDateToString(timeFrame));
 			}
-		}
-		return new CheckForDownloadResult(additionalAttributes, null);
+		}}
+		//System.out.println("errors end:"+errors);
+	
+		return new CheckForDownloadResult(additionalAttributes, null, deliveryCheck);
 
 	}
 
