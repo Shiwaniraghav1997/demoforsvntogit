c1b69633ae48e93408be887c8a37bcbbd97be39c
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-08-23 07:47:56 +0000

    OTT: 107550 : Material list is addedd
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1033 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/CheckForDownloadResult.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/CheckForDownloadResult.java
@@ -7,13 +7,26 @@ public class CheckForDownloadResult {
 
 	private Map<String, String> additionalAttributes;
 	private List<String> additionalObjectIds;
-
-	public CheckForDownloadResult(Map<String, String> additionalAttributes, List<String> additionalObjectIds) {
+	private List<String> materialList;
+	public CheckForDownloadResult(Map<String, String> additionalAttributes, List<String> additionalObjectIds, List<String> materialList) {
 		super();
 		this.additionalAttributes = additionalAttributes;
 		this.additionalObjectIds = additionalObjectIds;
+		this.materialList=materialList;
 	}
 	
+	public CheckForDownloadResult() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getMaterialList() {
+		return materialList;
+	}
+
+	public void setMaterialList(List<String> materialList) {
+		this.materialList = materialList;
+	}
+
 	public Map<String, String> getAdditionalAttributes() {
 		return additionalAttributes;
 	}
