8affa24a6c1e009fb8b89ece45c6ce61e00631e0
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-11-21 09:37:38 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1074 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/2_0_6/DOC41WEBUI/src/com/bayer/bhc/doc41webui/web/documents/UploadSuccessfulController.java
@@ -0,0 +1,25 @@
+package com.bayer.bhc.doc41webui.web.documents;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bayer.bhc.doc41webui.common.exception.Doc41BusinessException;
+import com.bayer.bhc.doc41webui.web.AbstractDoc41Controller;
+
+@Controller
+public class UploadSuccessfulController extends AbstractDoc41Controller {
+
+	
+	@RequestMapping(value="/documents/uploadsuccess",method = RequestMethod.GET)
+	public Map<String, String> uploadSuccess(@RequestParam String type, @RequestParam String uploadurl) throws Doc41BusinessException{
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("type", type);
+		map.put("uploadurl", uploadurl);
+		return map;
+	}
+}
