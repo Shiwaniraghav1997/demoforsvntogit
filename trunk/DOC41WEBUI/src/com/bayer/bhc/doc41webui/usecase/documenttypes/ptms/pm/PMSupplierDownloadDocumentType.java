c225d9fa2cb47b770c9253e42ec520ab643d1a69
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-12-14 08:35:21 +0000

    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1103 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/pm/PMSupplierDownloadDocumentType.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/ptms/pm/PMSupplierDownloadDocumentType.java
@@ -1,6 +1,6 @@
 package com.bayer.bhc.doc41webui.usecase.documenttypes.ptms.pm;
 
-import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -39,11 +39,13 @@ public abstract class PMSupplierDownloadDocumentType implements DownloadDocument
 		return true;
 	}
 
+	
 	@Override
 	public CheckForDownloadResult checkForDownload(Errors errors, DocumentUC documentUC, String customerNumber, String vendorNumber, String objectId, String customVersion, Date timeFrame, Map<String, String> attributeValues, Map<String, String> viewAttributes, int subType,String purchaseOrder) throws Doc41BusinessException {
 		boolean flag=true;
 		List<String> deliveryCheck = null;
-		ArrayList<String> pv_list = new ArrayList<String>();
+//		List<String> bom_check = new ArrayList<String>();
+		//ArrayList<String> pv_list = new ArrayList<String>();
 		if(subType==1)
 		{
 			flag=false;	
@@ -52,21 +54,26 @@ public abstract class PMSupplierDownloadDocumentType implements DownloadDocument
 		Doc41ValidationUtils.checkMaterialNumber(objectId, "objectId", errors, flag);
 	
 		Map<String, String> additionalAttributes = new HashMap<String, String>();
+		
 		if (!errors.hasErrors()) {
 			
 			if(subType==1) {
 				try {
-					 deliveryCheck = documentUC.checkSpecification(vendorNumber, purchaseOrder,customVersion);
-					/* System.out.println("deliveryCheck for specification::"+deliveryCheck.get(0));*/
+					 deliveryCheck = documentUC.checkSpecification(purchaseOrder,vendorNumber,customVersion, objectId);
 					 if (deliveryCheck.get(0) != null) {
+						 if( !deliveryCheck.get(0).equals("NoBomFound")) {
 							errors.reject("" + deliveryCheck.get(0));
+						 }
 						}
 
 						if (deliveryCheck.size() >= 2) {
 							for(int i=2;i<deliveryCheck.size();i++) {
 								if(deliveryCheck.get(i).length()==4){
-									pv_list.add( deliveryCheck.get(i));
-									additionalAttributes.put("IV_VERID_BOM", deliveryCheck.get(i));	
+									
+									//pv_list.add(deliveryCheck.get(i));
+									additionalAttributes.put("IV_VERID_BOM", deliveryCheck.get(i));
+									
+									
 								}
 								}}
 				} catch (Doc41ServiceException e) {
@@ -86,8 +93,7 @@ public abstract class PMSupplierDownloadDocumentType implements DownloadDocument
 				additionalAttributes.put("IV_TIMEFRAME_DATE", Doc41Utils.convertDateToString(timeFrame));
 			}
 		}}
-		//System.out.println("errors end:"+errors);
-	
+	//System.out.println("pmd deliveryCheck:"+deliveryCheck);
 		return new CheckForDownloadResult(additionalAttributes, null, deliveryCheck);
 
 	}
