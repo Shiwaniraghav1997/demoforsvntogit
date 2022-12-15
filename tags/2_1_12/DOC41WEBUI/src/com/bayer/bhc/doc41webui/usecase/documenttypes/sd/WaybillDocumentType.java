2fbeb9d56a480fae4ec1a469cb52c5b7988e8a4a
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2022-12-15 10:13:31 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1110 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/2_1_12/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/sd/WaybillDocumentType.java
@@ -0,0 +1,36 @@
+package com.bayer.bhc.doc41webui.usecase.documenttypes.sd;
+
+import java.util.Set;
+
+import com.bayer.bhc.doc41webui.usecase.documenttypes.DirectDownloadDocumentType;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
+
+
+public class WaybillDocumentType extends SDDocumentType implements DownloadDocumentType,DirectDownloadDocumentType {
+
+	@Override
+	public String getTypeConst() {
+		return "WB";
+	}
+
+	@Override
+	public String getSapTypeId() {
+		return "DOC41.15";
+	}
+
+	@Override
+	public String getPermissionDownload() {
+		return "DOC_WB_DOWN";
+	}
+	
+	@Override
+	public String getPermissionDirectDownload() {
+		return "DOC_WB_DIRECT_DOWN";
+	}
+
+	@Override
+	public Set<String> getMandatoryAttributes() {
+		return null;
+	}
+
+}
