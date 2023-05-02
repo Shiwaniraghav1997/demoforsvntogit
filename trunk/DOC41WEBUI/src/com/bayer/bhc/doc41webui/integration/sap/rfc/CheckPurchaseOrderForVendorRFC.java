055a8bdeecd078540918149b285a6b6e56ab96d2
elerj <elerj@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2023-05-02 10:17:05 +0000

    Code formated
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1122 7fa4df38-a670-11e2-8034-c9adf4be1a24

--- trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/integration/sap/rfc/CheckPurchaseOrderForVendorRFC.java
+++ trunk/DOC41WEBUI/src/com/bayer/bhc/doc41webui/integration/sap/rfc/CheckPurchaseOrderForVendorRFC.java
@@ -7,8 +7,6 @@ import com.bayer.bhc.doc41webui.common.logging.Doc41Log;
 import com.bayer.bhc.doc41webui.common.util.Doc41ValidationUtils;
 import com.bayer.ecim.foundation.basic.StringTool;
 import com.bayer.ecim.foundation.sap3.SAPException;
-import com.sap.conn.jco.JCoField;
-import com.sap.conn.jco.JCoFieldIterator;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoParameterList;
 import com.sap.conn.jco.JCoTable;
@@ -16,9 +14,9 @@ import com.sap.conn.jco.JCoTable;
 public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 	private static final String IN_VENDOR = "IV_VENDOR";
 	private static final String IN_PO = "IV_PONUMBER";
-	private static final String IN_PV ="IV_PVERSION";
-	private static final String IN_MAT="IV_MATNR";
-	//private static final String IN_FLAG="IV_FLAG";
+	private static final String IN_PV = "IV_PVERSION";
+	private static final String IN_MAT = "IV_MATNR";
+	// private static final String IN_FLAG="IV_FLAG";
 
 	private static final String OUT_RETURNCODE = "EV_RETURN";
 	private static final String OUT_FLAG = "EV_FLAG";
@@ -26,35 +24,26 @@ public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 
 	private static final String RETURNCODE_OK = "0";
 	private static final String RETURNCODE_PO_NOT_FOUND = "1";
-//	 private static final String RETURNCODE_NO_MAT_FOR_PO = "3";
+	// private static final String RETURNCODE_NO_MAT_FOR_PO = "3";
 	private static final String RETURNCODE_PV_NOT_FOUND = "2";
-	
-	/*For BomPLant added by elerj*/
+
+	/* For BomPLant added by elerj */
 	private static final String RETURNCODE_NO_BOM_PLANT = "3";
-	//private static final String RETURNCODE_NO_BOM_USAGE = "6";
+	 private static final String RETURNCODE_NO_VALID_PO = "4";
 
-
-	
 	// @Override
 	public void prepareCall(JCoFunction jCoFunction, List<?> inputParameterList) throws SAPException {
 		Doc41Log.get().debug(this, null, "ENTRY");
-		/*System.out.println("in Specification RFC call" + inputParameterList);*/
+
 		if (jCoFunction != null) {
 			if (inputParameterList != null) {
-				//System.out.println("purchaseOrderNumberll:"+inputParameterList.toString());
+
 				String vendorNumber = (String) inputParameterList.get(1);
 				String purchaseOrderNumber = (String) inputParameterList.get(0);
 				String productionVersion = (String) inputParameterList.get(2);
 				String materialNo = (String) inputParameterList.get(3);
-				//String flag =  (String) inputParameterList.get(4);
-//				System.out.println("purchaseOrderNumber:"+purchaseOrderNumber);
-//				System.out.println("vendorNumber:"+vendorNumber);
-//				System.out.println("productionVersion:"+productionVersion);
-//				System.out.println("materialNo:"+materialNo);
-//				System.out.println("flag:"+flag);
-				
-				
-
+				// String flag = (String) inputParameterList.get(4);
+				//
 				JCoParameterList sapInput = jCoFunction.getImportParameterList();
 				sapInput.setValue(IN_VENDOR, vendorNumber);
 				sapInput.setValue(IN_PV, productionVersion);
@@ -64,9 +53,7 @@ public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 				if (materialNo != null) {
 					sapInput.setValue(IN_MAT, materialNo);
 				}
-				/*if (flag != null) {
-					sapInput.setValue(IN_FLAG, flag);
-				}*/
+				
 				if (Doc41Log.get().isDebugActive()) {
 					Doc41Log.get().debug(this, null, RFCFunctionDumper.dumpFunction(jCoFunction));
 				}
@@ -80,7 +67,7 @@ public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 		Doc41Log.get().debug(this, null, "prepareCall():EXIT");
 	}
 
-	//@SuppressWarnings("unchecked")
+	// @SuppressWarnings("unchecked")
 	@Override
 	public List<String> processResult(JCoFunction pFunction) throws SAPException {
 		Doc41Log.get().debug(this, null, "processResult():ENTRY");
@@ -88,40 +75,32 @@ public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 		ArrayList<String> material_list = new ArrayList<String>();
 		ArrayList<String> pv_list = new ArrayList<String>();
 		ArrayList<String> Mat_Des = new ArrayList<String>();
-//		Map<String,ArrayList<Object>> multiMap = new HashMap<String, ArrayList<Object>>();
 		if (pFunction != null) {
 			if (Doc41Log.get().isDebugActive()) {
 				Doc41Log.get().debug(getClass(), null, RFCFunctionDumper.dumpFunction(pFunction));
 			}
 			JCoParameterList exportParameterList = pFunction.getExportParameterList();
 			String returnCode = exportParameterList.getString(OUT_RETURNCODE);
-		//	System.out.println("returnCode:"+returnCode);
-		//	returnCode="3";
-			//System.out.println(" after returnCode:"+returnCode);
-			
+
 			mResult.add(mapReturnCodeToTag(returnCode));
 			String outFlag = exportParameterList.getString(OUT_FLAG);
 			mResult.add(outFlag);
 			JCoTable outMaterial = pFunction.getTableParameterList().getTable(OUT_MATERIAL);
-			  
-			//JCoFieldIterator iter = outMaterial.getFieldIterator();
-			
-			for (int i = 0; i < outMaterial.getNumRows(); i++)
-		    {
+
+			for (int i = 0; i < outMaterial.getNumRows(); i++) {
 				outMaterial.setRow(i);
-				 // JCoField field = iter.nextField();
-				  String mat= (String) outMaterial.getValue("MATNR");
-				  material_list.add(mat);
-				  String P_iv= (String) outMaterial.getValue("P_VER");
-				  pv_list.add(P_iv);
-				  String MAt_Test= (String) outMaterial.getValue("MAKTX");
-				  Mat_Des.add(MAt_Test);
-				
-		    }
+				String mat = (String) outMaterial.getValue("MATNR");
+				material_list.add(mat);
+				String P_iv = (String) outMaterial.getValue("P_VER");
+				pv_list.add(P_iv);
+				String MAt_Test = (String) outMaterial.getValue("MAKTX");
+				Mat_Des.add(MAt_Test);
+
+			}
 			mResult.addAll(material_list);
-			  mResult.addAll(pv_list);
-			  mResult.addAll(Mat_Des);
-			
+			mResult.addAll(pv_list);
+			mResult.addAll(Mat_Des);
+
 		}
 		Doc41Log.get().debug(this, null, "EXIT");
 		return mResult;
@@ -132,18 +111,17 @@ public class CheckPurchaseOrderForVendorRFC extends AbstractDoc41RFC<String> {
 			return null;
 		} else if (StringTool.equals(returnCode, RETURNCODE_PO_NOT_FOUND)) {
 			return Doc41ValidationUtils.ERROR_MESSAGE_PO_NOT_FOUND;
-		}
-	 else if (StringTool.equals(returnCode, RETURNCODE_NO_BOM_PLANT)) {
+		} else if (StringTool.equals(returnCode, RETURNCODE_NO_BOM_PLANT)) {
 			return Doc41ValidationUtils.ERROR_MESSAGE_BOM_NOT_FOUND;
-		} 
-			 
-			  else if (StringTool.equals(returnCode, RETURNCODE_PV_NOT_FOUND)) {
+		}
+
+		else if (StringTool.equals(returnCode, RETURNCODE_PV_NOT_FOUND)) {
 			return Doc41ValidationUtils.ERROR_MESSAGE_PV_NOT_FOUND;
 		}
+		else if (StringTool.equals(returnCode, RETURNCODE_NO_VALID_PO)) {		
+			return Doc41ValidationUtils.ERROR_MESSAGE_CORRECT_VENDOR_NUM;		}
+		
 		return Doc41ValidationUtils.ERROR_MESSAGE_UNKNOWN_RETURN_CODE;
 	}
 
 }
-
-
-
