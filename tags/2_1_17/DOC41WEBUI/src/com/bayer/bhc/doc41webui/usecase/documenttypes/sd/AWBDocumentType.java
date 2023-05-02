660c4039fe0a1ce54116da0d7e4f6b0b82d45d41
myefs <myefs@7fa4df38-a670-11e2-8034-c9adf4be1a24>
2023-05-02 12:28:54 +0000

    Tag created by SvnTag Plugin. Build:null.
    git-svn-id: https://by-stsvn.bayer-ag.com/svn/DOC41WEBUI@1128 7fa4df38-a670-11e2-8034-c9adf4be1a24

---
+++ tags/2_1_17/DOC41WEBUI/src/com/bayer/bhc/doc41webui/usecase/documenttypes/sd/AWBDocumentType.java
@@ -0,0 +1,42 @@
+package com.bayer.bhc.doc41webui.usecase.documenttypes.sd;
+
+import java.util.Set;
+
+import com.bayer.bhc.doc41webui.usecase.documenttypes.DirectDownloadDocumentType;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.DownloadDocumentType;
+import com.bayer.bhc.doc41webui.usecase.documenttypes.UploadDocumentType;
+
+
+public class AWBDocumentType extends SDDocumentType implements DownloadDocumentType,UploadDocumentType,DirectDownloadDocumentType{
+
+	@Override
+	public String getTypeConst() {
+		return "AWB";
+	}
+
+	@Override
+	public String getSapTypeId() {
+		return "DOC41.16";
+	}
+
+	@Override
+	public String getPermissionUpload() {
+		return "DOC_AWB_UP";
+	}
+
+	@Override
+	public String getPermissionDownload() {
+		return "DOC_AWB_DOWN";
+	}
+
+	@Override
+	public String getPermissionDirectDownload() {
+		return "DOC_AWB_DIRECT_DOWN";
+	}
+
+	@Override
+	public Set<String> getMandatoryAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+}
